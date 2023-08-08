5f58e97ce9b1ce3c2a89d6d01f6caa0b19b18c3d
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-08-08 17:34:29 +0900

    페이지네이션을 위한 수정

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;    //페이지네이션 추가
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Paginator::useBootstrap();  //페이지네이션 추가
     }
 }
